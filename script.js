77480f2c74a9177c153bdc7f7b4d5b8b2917f9e6
Ryan Stephens <ryandstephens6@gmail.com>
2022-12-06 19:47:02 -0700

    Fetched location api

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-var requestUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=39.73&lon=104.99&appid=7590f1b542e088ea96b7e08570642814";
-console.log(requestUrl);
+var locationUrl = "http://api.openweathermap.org/geo/1.0/direct?q=Denver&limit=1&appid=7590f1b542e088ea96b7e08570642814"
+var weatherUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=39.73&lon=104.99&appid=7590f1b542e088ea96b7e08570642814&units=imperial";
 
-fetch(requestUrl)
+fetch(weatherUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    })
+
+fetch(locationUrl)
     .then(function (response) {
       return response.json();
     })
