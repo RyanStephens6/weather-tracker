38855edea3017ae32731543a093456d5ef7ba005
Ryan Stephens <ryandstephens6@gmail.com>
2022-12-06 20:37:29 -0700

    Added html for 5-day forecast

--- script.js
+++ script.js
@@ -1,22 +1,3 @@
-// var locationUrl = "http://api.openweathermap.org/geo/1.0/direct?q=Denver&limit=1&appid=7590f1b542e088ea96b7e08570642814"
-// var weatherUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=39.73&lon=104.99&appid=7590f1b542e088ea96b7e08570642814&units=imperial";
-
-// fetch(weatherUrl)
-//     .then(function (response) {
-//       return response.json();
-//     })
-//     .then(function (data) {
-//       console.log(data);
-//     })
-
-// fetch(locationUrl)
-//     .then(function (response) {
-//       return response.json();
-//     })
-//     .then(function (data) {
-//       console.log(data);
-//     })
-
 const submitButton = $(".submitButton");
 submitButton.click(function(e) {
     e.preventDefault();
@@ -40,6 +21,7 @@ function fetchApis(location) {
                 })
                 .then(function (data) {
                     console.log(data);
+                    
                 })
         })
 }
